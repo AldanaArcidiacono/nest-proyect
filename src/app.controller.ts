fdf4d6ddfb21a25b7970c97fc539953d58961a4e
AldanaArcidiacono <aldana.arcidiacono@gmail.com>
2024-04-20 15:25:53 +0200

    ✨ Add new controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('new')
+  newEndpoint(): string {
+    return 'New endpoint';
+  }
+
+  @Get('product/:prodId')
+  getProd(@Param('prodId') prodId: string): string {
+    return `Product id: ${prodId}`;
+  }
+
+  @Get('category/:id/products/:prodId')
+  getCategories(
+    @Param('id') id: string,
+    @Param('prodId') prodId: string,
+  ): string {
+    return `Product id: ${prodId} from category: ${id}`;
   }
 }
